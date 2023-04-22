b6ba6962bd14d2945dbdc716abe63bbb9c1b3513
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2023-04-22 20:43:12 +0300

    Refactored handle menu function

--- lab5/main.c
+++ lab5/main.c
@@ -44,7 +44,7 @@ void printLinkedFile(char filename[])
     printf("Size of target: %ld\n", buff.st_size);
 }
 
-int handleDirectory(char filename[])
+int countDirectoryCfiles(char filename[])
 {
     DIR *dir;
     dir = opendir(filename);
@@ -62,102 +62,125 @@ int handleDirectory(char filename[])
     return count;
 }
 
-void handleMenu(char filename[], struct stat buff)
+void handleRegularFile(char filename[], struct stat *buff, char options[])
+{
+    for (int i = 0; i < strlen(options); i++)
+    {
+        switch (options[i])
+        {
+        case 'n':
+            printf("%s\n", filename);
+            break;
+        case 'd':
+            printf("%ld\n", buff->st_size);
+            break;
+        case 'h':
+            printf("%ld\n", buff->st_nlink);
+            break;
+        case 'm':
+            printf("%s", ctime(&buff->st_mtime));
+            break;
+        case 'a':
+            printAccessRights(buff);
+            break;
+        case 'l':
+            createSymlink(filename);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void handleSymbolicLink(char filename[], struct stat *buff, char options[])
+{
+    for (int i = 0; i < strlen(options); i++)
+    {
+        switch (options[i])
+        {
+        case 'n':
+            printf("%s\n", filename);
+            break;
+        case 'l':
+            unlink(filename);
+            break;
+        case 'd':
+            printf("%ld\n", buff->st_size);
+            break;
+        case 'z':
+            printLinkedFile(filename);
+            break;
+        case 'a':
+            printAccessRights(buff);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void handleDirectory(char filename[], struct stat *buff, char options[])
+{
+    for (int i = 0; i < strlen(options); i++)
+        {
+            switch (options[i])
+            {
+            case 'n':
+                printf("%s\n", filename);
+                break;
+            case 'd':
+                printf("%ld\n", buff->st_size);
+                break;
+            case 'a':
+                printAccessRights(buff);
+                break;
+            case 'c':
+                printf("%d\n", countDirectoryCfiles(filename));
+                break;
+            default:
+                break;
+            }
+        }
+}
+
+void handleMenu(char filename[], struct stat *buff)
 {
     char input[10];
-    if (S_ISREG(buff.st_mode))
+    if (S_ISREG(buff->st_mode))
     {
         printf("Regular file: %s\nEnter options:\n-n (file name)\n-d(dim/size)\n-h (number of hard links)\n-m (time of last modif)\n-a (access rights)\n-l [filename] (create a symbolic link)\n", filename);
         fgets(input, 10, stdin);
         char options[10];
         sscanf(input, "-%10s", options);
-        for (int i = 0; i < strlen(options); i++)
-        {
-            switch (options[i])
-            {
-            case 'n':
-                printf("%s\n", filename);
-                break;
-            case 'd':
-                printf("%ld\n", buff.st_size);
-                break;
-            case 'h':
-                printf("%ld\n", buff.st_nlink);
-                break;
-            case 'm':
-                printf("%s", ctime(&buff.st_mtime));
-                break;
-            case 'a':
-                printAccessRights(&buff);
-                break;
-            case 'l':
-                createSymlink(filename);
-                break;
-            default:
-                break;
-            }
-        }
+        // TODO fork here
+        handleRegularFile(filename, buff, options);
     }
-    else if (S_ISLNK(buff.st_mode))
+    else if (S_ISLNK(buff->st_mode))
     {
         printf("Symbolic link: %s\nEnter options:\n-n (link name)\n-l (delete link)\n-d (size of link)\n-z (size of target)\n-a (access rights for symbolic link)\n", filename);
         fgets(input, 10, stdin);
         char options[10];
         sscanf(input, "-%10s", options);
-        for (int i = 0; i < strlen(options); i++)
-        {
-            switch (options[i])
-            {
-            case 'n':
-                printf("%s\n", filename);
-                break;
-            case 'l':
-                unlink(filename);
-                break;
-            case 'd':
-                printf("%ld\n", buff.st_size);
-                break;
-            case 'z':
-                printLinkedFile(filename);
-                break;
-            case 'a':
-                printAccessRights(&buff);
-                break;
-            default:
-                break;
-            }
-        }
+        handleSymbolicLink(filename, buff, options);
     }
-    else if (S_ISDIR(buff.st_mode))
+    else if (S_ISDIR(buff->st_mode))
     {
         printf("Directory: %s\nEnter options:\n-n (name)\n-d(dim/size)\n-a (access rights)\n-c (total number of .c files)\n", filename);
         fgets(input, 10, stdin);
         char options[10];
         sscanf(input, "-%10s", options);
-        for (int i = 0; i < strlen(options); i++)
-        {
-            switch (options[i])
-            {
-            case 'n':
-                printf("%s\n", filename);
-                break;
-            case 'd':
-                printf("%ld\n", buff.st_size);
-                break;
-            case 'a':
-                printAccessRights(&buff);
-                break;
-            case 'c':
-                printf("%d\n", handleDirectory(filename));
-                break;
-            default:
-                break;
-            }
-        }
+        handleDirectory(filename, buff, options);
     }
 }
-void handleCfile(char filename)
+void handleCfile(char filename[])
 {
+    printf("C file: %s\n", filename);
+    char *args[] = {"./compileScript.sh", filename, NULL};
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execvp(args[0], args);
+    }
 }
 int main(int argc, char *argv[])
 {
@@ -171,35 +194,12 @@ int main(int argc, char *argv[])
     for (int i = 1; i < argc; i++)
     {
         int result = lstat(argv[i], &buff);
-        // check if filename ends in .c
-        if (strstr(argv[i], ".c") != NULL)
+        if (result == -1)
         {
-            printf("C file: %s\n", argv[i]);
-            char *args[] = {"./compileScript.sh", argv[i], NULL};
-            pids[i - 1] = fork();
-            if (pids[i - 1] == 0)
-            {
-                execvp(args[0], args);
-            }
+            perror("lstat");
+            exit(EXIT_FAILURE);
         }
-        else if (result == 0)
-        {
-            pids[i - 1] = fork();
-            if (pids[i - 1] < 0)
-            {
-                printf("fork failed");
-                exit(EXIT_FAILURE);
-            }
-            else if (pids[i - 1] == 0)
-            {
-                handleMenu(argv[i], buff);
-                exit(EXIT_SUCCESS);
-            }
-        }
-        else
-        {
-            printf("could not read info about file");
-        }
+        handleMenu(argv[i], &buff);
     }
     for (int i = 0; i < argc - 1; i++)
     {
